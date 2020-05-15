4db60fb0c473a12aa1913630d813c81321569c82
Brotot Biswas <brototi.biswas1@gmail.com>
2020-05-15 18:40:58 -0400

    project with no image cover shows placeholder

--- src/js/main.js
+++ src/js/main.js
@@ -16,25 +16,21 @@ function getProjectData() {
         url: 'https://api.jsonbin.io/b/5ebef8bb8284f36af7bbc3d7/2',
         async: false,
         success: function(data) {
-            console.log('done')
             res = data;
         }
     });
 
-    console.log(res);
     return res;
 }
 
 
 function populateUI(project) {
     for(i = 0; i < project.length; i++) {
-        console.log("p",project)
-        // console.log(project.image_name, project.title, project.description, project.code_url)
         $('#PROJECTS')
         .append(
             `<div class="col-xs-12 col-sm-6 col-lg-3 col-xl-4 mb-4">
                 <div class="card box-shadow" style="width: 100%;">
-                    <img class="card-img-top" src="image/${project[i].image_name}.png" alt="Card image cap">
+                    <img class="card-img-top" src="image/${project[i].image_name}.png" alt="Card image cap" onerror="imageError(this);">
                     <div class="card-body">
                         <h5 class="card-title portfolio-title font-medium-bold">${project[i].title}</h5>
                         <p class="card-desc font-light">${project[i].description}</p>
@@ -51,4 +47,11 @@ function populateUI(project) {
             </div>`
         );
     }
+}
+
+
+function imageError(image) {
+    image.onrror = '';
+    image.src = 'image/placeholder.png';
+    return true;
 }
